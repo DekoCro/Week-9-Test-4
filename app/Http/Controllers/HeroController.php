a5c51e859564c2a349329e2e6f0f3058e57c5fbc
Andelko <aldeko96@gmail.com>
2019-03-04 10:05:05 +0100

    Finalizing

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Hero;
+use App\Image;
+use App\Emergency;
 
 class HeroController extends Controller
 {
@@ -18,5 +21,39 @@ class HeroController extends Controller
         $view = view('hero/show');
         $view->hero = $hero;
         return $view;
+    }
+
+    public function index()
+    {
+        $heroes = Hero::orderBy('name' , 'asc')->get();
+
+        return view('/hero/index', compact('heroes'));
+    }
+
+    public function create()
+    {
+        $emergency = new Emergency;
+        
+        //return view('hero/show', compact('emergency'));
+        return redirect()->route('homepage');
+    }
+
+    public function store(Request $request, $id)
+    {
+        $emergency = new Emergency;
+
+        $hero = Hero::findOrFail($id);
+
+        $emergency->fill($request->only([
+            'subject', 
+            'description'
+        ]));
+        $emergency->hero_id = $hero->id;
+
+        $emergency->save();
+
+        session()->flash('success_message', 'Success!');
+
+        return redirect()->route('emergency.create' , ['hero_slug' => $hero->name]);
     }
 }
